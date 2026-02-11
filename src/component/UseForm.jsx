f19f870b04facd1f57a8800fec8cf48cddf9d1d3
Sauravvaghela71 <sauravvaghela2004@gmail.com>
2026-02-11 20:39:15 +0530

    checkbox

--- src/component/UseForm.jsx
+++ src/component/UseForm.jsx
@@ -5,19 +5,19 @@ const UseForm = () => {
     const {register,handleSubmit} = useForm()
     const [userdata, setUserdata] = useState({})
     const [isset, setIsset] = useState(false)
-
+    const [checked,setChecked] = useState(false);
     const submithandler=(data)=>{
+
+      
         console.log(data);
         setUserdata(data);
-        setIsset(true)
-        
     }
   
 
     
   return (<>
 
-    <form style={{textAlign:"center"}} onSubmit={handleSubmit(submithandler)}>
+    <form className="secondary" style={{textAlign:"center"}} onSubmit={handleSubmit(submithandler)}>
         <h2>UseForm</h2>
        Name : <input type="text" placeholder='enter name'{...register("name")} /> <br/>
        age : <input type="text" placeholder='enter age' {...register("age")}/> <br/>
@@ -25,13 +25,16 @@ const UseForm = () => {
             <input type="radio" value="male" {...register("gender")}/>male<br/>
             <input type="radio" value="female"  {...register("gender")}/>female<br/>
 
-       <label > country</label>
+       <label > country : </label>
        <select {...register("country")}>
        <option value="india" {...register("india")}>INDIA</option>
         <option value="pakistana" {...register("pakistana")}>pakistan</option>
          <option value="bangladesh" {...register("bangladesh")}>bangladesh</option>
-       </select>
-       <button type='submit'>submit</button>
+       </select><br/>
+
+        <input type="checkbox" onClick={()=>setChecked(!checked)} /> please check term and condision<br/>
+       
+       <button type='submit'  disabled={!checked}>submit</button>
 
     </form>
 
@@ -41,7 +44,7 @@ const UseForm = () => {
     <h3>name:{userdata.name}</h3>
     <h3>age:{userdata.age}</h3>
     <h3>gender:{userdata.gender}</h3>
-    <h3>country{userdata.country}</h3>
+    <h3>country:{userdata.country}</h3>
     </>}
 
 
