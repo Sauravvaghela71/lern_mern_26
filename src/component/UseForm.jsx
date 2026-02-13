00e242890eec618b12a7eff911d42ebc0ceada6f
Sauravvaghela71 <sauravvaghela2004@gmail.com>
2026-02-13 17:25:45 +0530

    use-Form-validation

--- src/component/UseForm.jsx
+++ src/component/UseForm.jsx
@@ -2,15 +2,56 @@ import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 const UseForm = () => {
-    const {register,handleSubmit} = useForm()
+    const {register,handleSubmit, formState:{errors}} = useForm()
     const [userdata, setUserdata] = useState({})
     const [isset, setIsset] = useState(false)
     const [checked,setChecked] = useState(false);
-    const submithandler=(data)=>{
 
-      
+    const validationschema = {
+       namevalidation:{
+        required:{
+          value:true,
+          message:"name is require"
+        }
+        },
+        agevalidation:{
+          required:{
+            value:true,
+            message:"age is require"
+          
+          },
+          max:{
+            value:100,
+            message:"age not allow above 100"
+          },
+          min:{
+            value:18,
+            message:"age above 18 "
+          }
+        },
+        gendervalidation:{
+          required:{
+            value:true,
+            message:"plese check the feild"
+          }
+        },
+        countryvalidate:{
+          required:{
+            value:true,
+            message:"please select country"
+          }
+        }
+
+    }
+
+
+
+
+
+    const submithandler=(data)=>{
         console.log(data);
         setUserdata(data);
+        setIsset(true)
     }
   
 
@@ -19,22 +60,37 @@ const UseForm = () => {
 
     <form className="secondary" style={{textAlign:"center"}} onSubmit={handleSubmit(submithandler)}>
         <h2>UseForm</h2>
-       Name : <input type="text" placeholder='enter name'{...register("name")} /> <br/>
-       age : <input type="text" placeholder='enter age' {...register("age")}/> <br/>
-         <label >gender :</label> <br/>
-            <input type="radio" value="male" {...register("gender")}/>male<br/>
-            <input type="radio" value="female"  {...register("gender")}/>female<br/>
+        
+       Name : <input type="text" placeholder='enter name'{...register("name", validationschema.namevalidation)} /> <br/>
+              <p style={{color:"red"}}>{errors.name?.message}</p>
+
+              
+       age : <input type="text" placeholder='enter age' {...register("age",validationschema.agevalidation)}/> <br/>
+              <p style={{color:"red"}}>{errors.age?.message}</p>
+
+
+       <label >gender :</label> <br/>
+
+            <input type="radio" value="male" {...register("gender",validationschema.gendervalidation)}/>male<br/>
+            <input type="radio" value="female"  {...register("gender",validationschema.gendervalidation)}/>female<br/>
+            <p style={{color:"red"}}>{errors.gender?.message}</p>
 
        <label > country : </label>
-       <select {...register("country")}>
-       <option value="india" {...register("india")}>INDIA</option>
-        <option value="pakistana" {...register("pakistana")}>pakistan</option>
-         <option value="bangladesh" {...register("bangladesh")}>bangladesh</option>
+
+       <select {...register("country",validationschema.countryvalidate)}>
+              <option value="">---select country---</option>
+              <option value="india" {...register("india")}>INDIA</option>
+              <option value="pakistana" {...register("pakistana")}>pakistan</option>
+              <option value="bangladesh" {...register("bangladesh")}>bangladesh</option>
        </select><br/>
+              <p style={{color:"red"}}>{errors.country?.message}</p>
+
 
         <input type="checkbox" onClick={()=>setChecked(!checked)} /> please check term and condision<br/>
        
-       <button type='submit'  disabled={!checked}>submit</button>
+       
+        <button type='submit'>submit</button>
+
 
     </form>
 
